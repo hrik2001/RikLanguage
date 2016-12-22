19847b2292d5aae1670bda9c223846908dcf8256
Rik <rik61072@gmail.com>
2016-12-22 19:40:21 +0530

    added standard output functions instead of print

--- shell.py
+++ shell.py
@@ -6,7 +6,7 @@ import rikprogramming
 import sys
  
 def shell():
-	print "Rik Shell (REPL) V2.1 BETA, Written by Shatabarto Bhattacharya\n Type help for help, author and about to know about this project"
+	sys.stdout.write( "Rik Shell (REPL) V2.1 BETA, Written by Shatabarto Bhattacharya\n Type help for help, author and about to know about this project\n")
 	about='this is just a toy language built by Rik (Shatabarto Bhattacharya). Just for leisure. '
 	data=''
 	helps="press ^C to escape from a loop and ^D to quit"
@@ -18,11 +18,11 @@ def shell():
 		if r=='quit':
 			break
 		elif r=='author':
-			print "Shatabarto \'Rik\' Bhattacharya"
+			sys.stdout.write( "Shatabarto \'Rik\' Bhattacharya\n")
 		elif r=='about':
-			print about
+			sys.stdout.write( about+'\n')
 		elif r=='help':
-			print helps
+			sys.stdout.write( helps+'\n')
 		elif r=='open file' or r=='run file':
 			a=raw_input('enter da file name')
 			b=open(a,'r').read()
@@ -33,7 +33,7 @@ def shell():
 			try:
 			 	run(r+'\n')
 		 	except:
-		 	 	print "something went wrong, please ensure you code is correct"
+		 	 	sys.stdout.write( "something went wrong, please ensure you code is correct")
 
 try:
 	rikprogramming.run(open(sys.argv[1],"r").read())
